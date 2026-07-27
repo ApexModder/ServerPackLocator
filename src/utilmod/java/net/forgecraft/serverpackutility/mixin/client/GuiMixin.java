3d6c1e4ee5c98ee24e70ba2567e8199078011082
Apex <robsonlawson3@gmail.com>
2026-07-27 21:45:41 +0100

    Shadow `Minecraft` field

--- src/utilmod/java/net/forgecraft/serverpackutility/mixin/client/GuiMixin.java
+++ src/utilmod/java/net/forgecraft/serverpackutility/mixin/client/GuiMixin.java
@@ -11,16 +11,20 @@ import net.minecraft.client.main.GameConfig;
 import net.minecraft.client.multiplayer.resolver.ServerAddress;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(Gui.class)
 public abstract class GuiMixin {
+    @Shadow
+    private Minecraft minecraft;
+
     @WrapMethod(method = "buildInitialScreens")
     private Runnable serverpacklocator$buildInitialScreens(@Nullable GameLoadCookie cookie, Operation<Runnable> operation) {
         // parse server address
         var serverAddress = ServerAddress.parseString(ModAccessor.getQuickPlayServer());
 
         if ((cookie == null || !cookie.quickPlayData().isEnabled()) && !serverAddress.getHost().equals("server.invalid")) {
-            RealmsClient realmsclient = RealmsClient.getOrCreate(Minecraft.getInstance());
+            RealmsClient realmsclient = RealmsClient.getOrCreate(minecraft);
             cookie = new GameLoadCookie(realmsclient, new GameConfig.QuickPlayData(
                 null,
                 new GameConfig.QuickPlayMultiplayerData(serverAddress.toString())
